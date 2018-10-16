0fefbf2e819aa9e72b26fb7d75effe64e9f6f538
joseph_rubio <joseph.rubio@beitech.co>
2018-10-16 10:28:53 -0500

    creacion del proyecto

---
+++ clustering_microservice_api/src/main/java/com/ucatolica/clustering_microservice_api/ClusteringMicroserviceApiApplication.java
@@ -0,0 +1,12 @@
+package com.ucatolica.clustering_microservice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusteringMicroserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusteringMicroserviceApiApplication.class, args);
+	}
+}
---
+++ clustering_microservice_api/src/test/java/com/ucatolica/clustering_microservice_api/ClusteringMicroserviceApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ucatolica.clustering_microservice_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClusteringMicroserviceApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
